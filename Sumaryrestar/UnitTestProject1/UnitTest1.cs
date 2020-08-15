eb825bac6467534979dd516044e2d93554956d86
Flavia Larrain <a2019110032@estudiantes.upsa.edu.bo>
2020-08-15 12:29:40 -0400

    Mi primer unittest

---
+++ Sumaryrestar/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sumar.Controllers;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            //Arrange
+            SumaController sumaController = new SumaController();
+            int a = 5;
+            int b = 8;
+            int esperado = 13;
+
+            //Act
+            int resultado = sumaController.Add(a, b);
+            //Assert
+            Assert.AreEqual(esperado, resultado);
+        }
+    }
+}
